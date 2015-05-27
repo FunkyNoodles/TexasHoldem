ad0ccf9f77ff26276dd449130c8e8b97e138943e
nickasds <nickasds@hotmail.com>
2015-05-27 16:57:04 -0700

    First Commit

---
+++ Texas Holdem/src/texasHoldem/TexasHoldemMain.java
@@ -0,0 +1,10 @@
+/*
+ * From reddit.com/r/DailyProgrammer Challenger #216, 3 parts
+ */
+package texasHoldem;
+
+public class TexasHoldemMain {
+	public static void main(String[] args){
+		Game go = new Game();
+	}
+}
